09f166e079191482e26af7f6577b6a22c147928b
avistheripper <bboyxns@mail.ru>
2019-03-03 19:02:45 +0300

    Setting ws server

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -11,5 +19,19 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf("Response")
+	// fmt.Fprintf(w, "Response")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(msg))
+		err = socket.WriteMessage(msgType, msg)
+	}
 }
