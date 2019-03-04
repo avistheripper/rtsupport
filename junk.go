4c064274e88cba8ee4facf765523e8db5df614c8
avistheripper <bboyxns@mail.ru>
2019-03-04 22:07:09 +0300

    Setting go routine + channel

--- junk.go
+++ junk.go
@@ -1,51 +1,30 @@
-package main
+// package main
 
-import (
-	"encoding/json"
-	"fmt"
+// import (
+// 	"encoding/json"
+// 	"fmt"
 
-	"github.com/mitchellh/mapstructure"
-)
+// )
 
-type Message struct {
-	Name string      `json: "name"`
-	Data interface{} `json: "data"`
-}
+// func main() {
+// 	recRawMsg := []byte(`{"name": "channel add", ` + `"data":{"name": "Hardware support"}}`)
+// 	var recMessage Message
+// 	err := json.Unmarshal(recRawMsg, &recMessage)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	if recMessage.Name == "channel add" {
+// 		channel, err := addChannel(recMessage.Data)
+// 		var sendMessage Message
+// 		sendMessage.Name = "channel add"
+// 		sendMessage.Data = channel
+// 		sendRawMsg, err := json.Marshal(sendMessage)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		fmt.Print(string(sendRawMsg))
+// 	}
 
-type Channel struct {
-	Id   string `json: "id"`
-	Name string `json: "name"`
-}
-
-func main() {
-	recRawMsg := []byte(`{"name": "channel add", ` + `"data":{"name": "Hardware support"}}`)
-	var recMessage Message
-	err := json.Unmarshal(recRawMsg, &recMessage)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	if recMessage.Name == "channel add" {
-		channel, err := addChannel(recMessage.Data)
-		var sendMessage Message
-		sendMessage.Name = "channel add"
-		sendMessage.Data = channel
-		sendRawMsg, err := json.Marshal(sendMessage)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Print(string(sendRawMsg))
-	}
-
-}
-
-func addChannel(data interface{}) (Channel, error) {
-	var channel Channel
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return channel, err
-	}
-	channel.Id = "1"
-	return channel, nil
-}
+// }
